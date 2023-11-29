9014fdf4e75ca6e3e4a5a319fca7b07e7992d976
María A. Rodríguez <72228855+MariaRod123@users.noreply.github.com>
2023-11-29 14:27:47 -0300

    Delete src directory

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import ListaProductos from './components/ListaProductos.vue';
-
-createApp(App).mount('#app')
-App.component('ListaProductos', ListaProductos);
